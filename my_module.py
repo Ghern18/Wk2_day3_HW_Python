563e66f197bca2f9e8be0ce5c8b3aecb51fa5f93
Gabriela Hernandez <gabbyh1896@gmail.com>
2023-12-10 23:25:46 -0700

    first commit

---
+++ my_module.py
@@ -0,0 +1,9 @@
+import my_module.py
+
+import math
+
+def calculate_square_footage(length, width):
+    return length * width
+
+def calculate_circumference(radius):
+    return 2 * math.pi * radius
